561b15feae05f0d4ec19474fd57724fc6ad6ffb2
Sam Ardis <ardis.productions@gmail.com>
2020-04-21 13:10:48 -0400

    Added major file code, downloaded dependencies, working on migration and knexfile

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+
+const userRouter = require("./userRouter");
+const authRouter = require("./authRouter");
+const authenticator = require("./authenticator");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use("/api/users", authenticator, userRouter);
+server.use("/api/auth", authRouter);
+
+server.get("/", (req,res) => {
+    res.json({api: "up"});
+})
+
+module.exports = server; 
